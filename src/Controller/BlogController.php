cdc3cbdc5de5eba4adfd5b93745df0eb6c0639ae
Riccardo Fiorenza <ricfio.professional@gmail.com>
2021-05-01 21:01:52 +0200

    Route Groups and Prefixes

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,13 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/blog", requirements={"_locale": "en|es|fr"}, name="blog_")
+ */
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog/{page}", name="blog_list", defaults={"page": 1, "title": "Hello world!"})
+     * @Route("/{_locale}", name="index")
+     */
+    public function index(Request $request): Response
+    {
+        // ...
+        return new Response(
+            "<html><body>Blog index<br>- locale: {$request->getLocale()}</body></html>"
+        );
+    }
+
+    /**
+     * @Route("/{page}", name="list", requirements={"page"="\d+"}, defaults={"page": 1, "title": "Hello world!"})
      */
     public function list(int $page): Response
     {
@@ -22,7 +37,7 @@ class BlogController extends AbstractController
     /**
      * This route has a greedy pattern and is defined first.
      * 
-     * @Route("/blog/{slug}", name="blog_show")
+     * @Route("/{slug}", name="show")
      */
     public function show(string $slug): Response
     {
