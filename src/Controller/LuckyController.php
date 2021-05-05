9d08145b1a6f80c1f1ce4892f6d900cd0a4fa4bb
Riccardo Fiorenza <ricfio.professional@gmail.com>
2021-05-05 13:54:42 +0200

    Fetching Services

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,6 +2,7 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,9 +12,11 @@ class LuckyController extends AbstractController
     /**
      * @Route("/lucky/{max}", name="lucky_number")
      */
-    public function number(int $max = 100): Response
+    public function number(int $max = 100, LoggerInterface $logger): Response
     {
         $number = random_int(1, $max);
+
+        $logger->info("The random number extracted is {$number}");
 
         return $this->render('lucky/number.html.twig', [
             'number' => $number,
